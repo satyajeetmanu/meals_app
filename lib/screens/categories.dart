466204a16002b4dddadd1bc67c61970c2fb66e80
Satyajeet Gaurav <satyajeetmanu@gmail.com>
2023-10-04 15:30:59 +0530

    passed data to meals screen to display meals according to the chosen category

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -2,14 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
+import 'package:meals_app/models/category.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _onSelectCategory(BuildContext context) {
+  void _onSelectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
     Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (ctx) => MealsScreen(title: '', meals: []),
+        builder: (ctx) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,
+        ),
       ),
     );
   }
@@ -31,7 +38,7 @@ class CategoriesScreen extends StatelessWidget {
             .map((category) => CategoryGridItem(
                   category: category,
                   onSelectCategory: () {
-                    _onSelectCategory(context);
+                    _onSelectCategory(context, category);
                   },
                 ))
             .toList(),
